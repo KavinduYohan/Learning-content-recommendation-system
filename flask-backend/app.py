4c4ad91757d8fcc4998a4bf7322a8decb5dbb39b
KavinduYohan <kavindumarasinghe70@gmail.com>
2025-03-03 19:35:56 +0530

    Update database password and add session files for enhanced security

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -26,7 +26,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password",
+        password="Kavindu1495?",
         database="learningContent3"
     )
 
